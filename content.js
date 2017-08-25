88b3762d204a63b136cd998637069b2389ab87a2
Infocatcher <infocatcher.dev.null@gmail.com>
2017-08-25 23:01:30 +0300

    Draft for click handlers

--- content.js
+++ content.js
@@ -1,9 +1,22 @@
 function init() {
 	browser.runtime.onMessage.addListener(onMessageFromBackgroundScript);
+	window.addEventListener("mousedown", onMouseDown, true);
+	window.addEventListener("mouseup", onMouseUp, true);
+	window.addEventListener("click", onClick, true);
+	window.addEventListener("contextmenu", onContextMenu, true);
 }
 function destroy() {
 	browser.runtime.onMessage.removeListener(onMessageFromBackgroundScript);
+	window.removeEventListener("mousedown", onMouseDown, true);
+	window.removeEventListener("mouseup", onMouseUp, true);
+	window.removeEventListener("click", onClick, true);
+	window.removeEventListener("contextmenu", onContextMenu, true);
 }
 
 function onMessageFromBackgroundScript(msg) {
 }
+
+function onMouseDown(e) {}
+function onMouseUp(e) {}
+function onClick(e) {}
+function onContextMenu(e) {}
