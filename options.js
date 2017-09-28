309d5a83f8425fabdbc3ed255c02590e29974e7f
Infocatcher <infocatcher.dev.null@gmail.com>
2017-09-28 17:09:17 +0300

    Update options page, if changed externally

--- options.js
+++ options.js
@@ -16,17 +16,23 @@ var prefs = { // Defaults
 
 function loadOptions() {
 	browser.storage.local.get().then(function(o) {
+		browser.storage.onChanged.addListener(function(changes, area) {
+			if(area == "local") for(var key in changes)
+				loadOption(key, changes[key].newValue);
+		});
 		Object.assign(prefs, o);
-		for(var id in prefs) {
-			var node = document.getElementById(id);
-			if(!node)
-				continue;
-			if(node.type == "checkbox")
-				node.checked = prefs[id];
-			else
-				node.value = prefs[id];
-		}
+		for(var id in prefs)
+			loadOption(id, prefs[id]);
 	});
+}
+function loadOption(id, val) {
+	var node = document.getElementById(id);
+	if(!node)
+		return;
+	if(node.type == "checkbox")
+		node.checked = val;
+	else
+		node.value = val;
 }
 function saveOptions() {
 	var nodes = document.querySelectorAll("[id]");
