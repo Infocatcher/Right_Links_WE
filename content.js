Infocatcher/Right_Links_WE

commit 5a4c10c392b226fd9aa98fce25b12605762e999c

Tweak mousemove handling

content.js
@@ -163,21 +163,21 @@ function onContextMenu(e) {
 		stopEvent(e);
 }
 function onMouseMove(e) {
-	var mmd = moveHandlers.data;
-	if(!mmd.enabled)
+	var md = moveData;
+	if(!md.enabled)
 		return;
 	var x = e.screenX;
 	var y = e.screenY;
-	mmd.dist += Math.sqrt(
-		Math.pow(mmd.screenX - x, 2) +
-		Math.pow(mmd.screenY - y, 2)
+	md.dist += Math.sqrt(
+		Math.pow(md.x - x, 2) +
+		Math.pow(md.y - y, 2)
 	);
-	if(mmd.dist >= prefs.disallowMousemoveDist) {
+	if(md.dist >= prefs.disallowMousemoveDist) {
 		cancel(e);
 		return;
 	}
-	mmd.screenX = x;
-	mmd.screenY = y;
+	md.x = x;
+	md.y = y;
 }
 
 function isLeft(e) {
@@ -253,31 +253,31 @@ function showContextMenu(trg, origEvt) {
 	mouseEvents(trg, events, origEvt, {}); // Actually doesn't work...
 	blinkNode(trg);
 }
+var moveData = null;
 function moveHandlers(e) {
-	if(!e == !moveHandlers.data)
+	if(!e == !moveData)
 		return;
-	if(e) {
-		var dist = prefs.disallowMousemoveDist;
-		moveHandlers.data = {
-			enabled: dist >= 0,
-			dist: 0,
-			screenX: e.screenX,
-			screenY: e.screenY
-		};
-		listen(true, {
-			mousemove: onMouseMove,
-			wheel:     cancel,
-			dragstart: cancel
-		});
-	}
-	else {
-		moveHandlers.data = null;
+	if(!e) {
+		moveData = null;
 		listen(false, {
 			mousemove: onMouseMove,
 			wheel:     cancel,
 			dragstart: cancel
 		});
+		return;
 	}
+	var dist = prefs.disallowMousemoveDist;
+	moveData = {
+		enabled: dist >= 0,
+		dist: 0,
+		screenX: e.screenX,
+		screenY: e.screenY
+	};
+	listen(true, {
+		mousemove: onMouseMove,
+		wheel:     cancel,
+		dragstart: cancel
+	});
 }
 function resetFlags() {
 	flags.stopMouseUp = false;

commit 7fe4581b7d7245d8510749be66a83fbabff0507e

Fix mousemove handling (broken in 5a4c10c392b226fd9aa98fce25b12605762e999c commit)

content.js
@@ -277,8 +277,8 @@ function moveHandlers(e) {
 	moveData = {
 		enabled: dist >= 0,
 		dist: 0,
-		screenX: e.screenX,
-		screenY: e.screenY
+		x: e.screenX,
+		y: e.screenY
 	};
 	listen(true, {
 		mousemove: onMouseMove,